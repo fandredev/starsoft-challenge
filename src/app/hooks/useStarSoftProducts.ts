bb3942dd30e2a064c57c53d8b37196cfeba2d2af
felipe <profissionalf.andre@gmail.com>
2024-10-24 20:45:30 -0300

    feat: increment React infnite query to load more data products from API

--- src/app/hooks/useStarSoftProducts.ts
+++ src/app/hooks/useStarSoftProducts.ts
@@ -1,24 +1,39 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Product from '../interfaces/Product';
 
 const PRODUCTS_URL =
-  'https://starsoft-challenge-7dfd4a56a575.herokuapp.com/v1/products?page=1&limit=8';
+  'https://starsoft-challenge-7dfd4a56a575.herokuapp.com/v1/products';
 
-const searchAllProducts = async (): Promise<{ data: Product[] }> => {
-  const response = await fetch(PRODUCTS_URL);
+const MAXIMUM_QUANTITY_API_PAGES = 4; // https://starsoft-challenge-7dfd4a56a575.herokuapp.com/v1/docs#/default/ProductsController_findAll
 
+const searchAllProducts = async (
+  pageParam: number
+): Promise<{ data: Product[] }> => {
+  const response = await fetch(`${PRODUCTS_URL}?page=${pageParam}&limit=10`);
+  if (!response.ok) {
+    throw new Error('Erro ao buscar os produtos');
+  }
   return await response.json();
 };
 
 const useStarSoftProducts = () => {
-  const query = useQuery({
-    queryKey: ['products'],
-    queryFn: searchAllProducts,
-  });
+  const { data, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['products'],
+      queryFn: ({ pageParam = 1 }) => searchAllProducts(pageParam),
+      initialPageParam: 1,
+      getNextPageParam: (_lastPage, pages) => {
+        if (pages.length < MAXIMUM_QUANTITY_API_PAGES) return pages.length + 1;
+        return undefined;
+      },
+    });
 
   return {
-    ...query,
-    data: query.data?.data,
+    data,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
   };
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Fragment } from 'react';
 import CardProduct from './components/ui/CardProduct';
 import Footer from './components/ui/Footer';
 import HeaderHomePage from './components/ui/Header';
@@ -9,7 +10,8 @@ import { useStarSoftProducts } from './hooks/useStarSoftProducts';
 import styles from './Page.module.scss';
 
 export default function Home() {
-  const { data, isLoading } = useStarSoftProducts();
+  const { data, isLoading, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useStarSoftProducts();
 
   return (
     <div>
@@ -17,10 +19,30 @@ export default function Home() {
       {!isLoading && (
         <>
           <main className={styles.main}>
-            {data?.map((product) => (
-              <CardProduct key={product.id} product={product} />
-            ))}
+            {data?.pages.map((groupPages, index) => {
+              return (
+                <Fragment key={index}>
+                  {groupPages.data.map((product) => (
+                    <CardProduct key={product.id} product={product} />
+                  ))}
+                </Fragment>
+              );
+            })}
           </main>
+
+          <section className={styles.section}>
+            {hasNextPage && (
+              <button
+                disabled={!hasNextPage || isFetchingNextPage}
+                onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                  e.preventDefault();
+                  fetchNextPage();
+                }}
+              >
+                {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+              </button>
+            )}
+          </section>
 
           <Footer />
         </>
